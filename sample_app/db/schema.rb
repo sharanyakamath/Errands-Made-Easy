c01d4b84f61a65c41acaed33d55901997dc14658
sharanyakamath <sherrykamath@gmail.com>
2018-08-17 22:11:03 +0530

    added password authentication

--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_17_070222) do
+ActiveRecord::Schema.define(version: 2018_08_17_161803) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
