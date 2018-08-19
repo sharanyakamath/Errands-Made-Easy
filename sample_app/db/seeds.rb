0cf6ef77fcb414ea44ae531ac1b0345eb7067bb1
sharanyakamath <sherrykamath@gmail.com>
2018-08-19 19:59:53 +0530

    complaint feature added

--- sample_app/db/seeds.rb
+++ sample_app/db/seeds.rb
@@ -6,3 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create!(name: "Admin",email: "admin@nitk.edu.in",password:"123456",password_confirmation: "123456",admin: true)
+users = User.order(:created_at).take(6)
+content = Faker::Lorem.sentence(5)
+users.each { |user| user.microposts.create!(content: content) }
