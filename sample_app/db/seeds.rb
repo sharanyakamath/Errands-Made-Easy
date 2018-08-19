c5dfb87cf978e6a718d5cad6923cebc5c2a94373
sharanyakamath <sherrykamath@gmail.com>
2018-08-19 15:08:32 +0530

    Admin added

--- sample_app/db/seeds.rb
+++ sample_app/db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: "Admin",email: "admin@nitk.edu.in",password:"123456",password_confirmation: "123456",admin: true)
