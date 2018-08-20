ab85136938e057f24d66fdd6b95998dc4f16f13a
sharanyakamath <sherrykamath@gmail.com>
2018-08-20 09:59:33 +0530

    complaint resolved

--- sample_app/app/controllers/microposts_controller.rb
+++ sample_app/app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :admin_user, only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -13,10 +14,18 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost = Micropost.find_by(id: params[:id])
+    @micropost.destroy
+    flash[:success] = "Complaint Resolved"
+    redirect_to request.referrer || root_url
   end
 
   private
     def micropost_params
       params.require(:micropost).permit(:content)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
